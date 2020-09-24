d189c22fdbf1384e0756a897fc16b99517861505
Zak <rusty_clarinet@yahoo.com>
2020-09-24 16:14:22 -0400

    Initial commit

---
+++ Assets/_Script/Listener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour
+{
+
+    
+    public void Printing(float something)
+    {
+        something = 10;
+        print("NoteInput play note");
+        
+    }
+}
