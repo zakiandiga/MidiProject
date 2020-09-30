3bf8404f30db1797cbd94fff0ad0c73a46c871fd
FSimas92 <felipesimas92@msn.com>
2020-09-30 13:05:54 -0400

    FelipeTesting scene assigning klak nodes to kino effects

---
+++ Assets/FelipeStuff/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using Kino;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private AnalogGlitch analogGlitch;
+
+    public GameObject mainCam;
+    bool contourOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        analogGlitch = mainCam.GetComponent<AnalogGlitch>();
+
+        contourOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            setScanLineJitter();
+        }
+    }
+
+    public void setScanLineJitter()
+    {
+        analogGlitch.scanLineJitter = 1;
+    }
+
+    public void setContour()
+    {
+        if (contourOn == false)
+        {
+            mainCam.GetComponent<Contour>().enabled = true;
+            contourOn = true;
+        } else
+        {
+            mainCam.GetComponent<Contour>().enabled = false;
+            contourOn = false;
+        }
+        
+        
+    }
+}
